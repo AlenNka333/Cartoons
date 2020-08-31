cc286c57e5c729e8338caf4ae70adda4e7d47228
Alena <elena.walst@bk.ru>
2020-08-31 10:05:39 +0300

    Initial commit

---
+++ Cartoons/Cartoons/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Cartoons
+//
+//  Created by Alena Nesterkina on 8/29/20.
+//  Copyright © 2020 AlenaNesterkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
