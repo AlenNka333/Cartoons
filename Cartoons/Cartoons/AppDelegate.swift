b4b8f29045fa8e15c932e5245abf2de5f5ad485f
Alena <elena.walst@bk.ru>
2020-09-01 17:01:56 +0300

    Add Localizable.strings file

--- Cartoons/Cartoons/AppDelegate.swift
+++ Cartoons/Cartoons/AppDelegate.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2020 AlenaNesterkina. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
