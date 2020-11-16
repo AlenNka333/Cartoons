cf3a452187e6de3c816403255fe3e11a657037af
Alena <elena.walst@bk.ru>
2020-11-16 19:03:11 +0300

    - disable paste in phone number and vCode textFields

--- Cartoons/Cartoons/Components/Custom/UIKit/TextField/CustomTextField.swift
+++ Cartoons/Cartoons/Components/Custom/UIKit/TextField/CustomTextField.swift
@@ -19,6 +19,13 @@ class CustomTextField: UITextField {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+            return false
+        }
+        return super.canPerformAction(action, withSender: sender)
+    }
+    
     func setupTextField() {
         keyboardType = .numberPad
         borderStyle = .none
